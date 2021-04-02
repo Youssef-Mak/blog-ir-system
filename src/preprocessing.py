219d4a70b0d43d7b5ba5830331ec5d8862c92a28
Youssef-Mak <youssef.mak10@gmail.com>
2021-04-02 18:37:32 -0400

    Polished docstrings and styling

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,3 +1,4 @@
+from nltk.corpus import stopwords
 import nltk
 try:
     nltk.data.find('tokenizers/punkt')
@@ -17,48 +18,53 @@ import pandas as pd
 from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
 
-from nltk.corpus import stopwords
 stop_words = set(stopwords.words("english"))
 
 trec_microblog_src = 'data/Trec_microblog11.txt'
-trec_mb_df = pd.read_csv(trec_microblog_src, delimiter='\t', names=["id", "raw_message"])
+trec_mb_df = pd.read_csv(trec_microblog_src, delimiter='\t', names=[
+                         "id", "raw_message"])
 
 # Convert all messages to lowercase
 trec_mb_df['raw_message'] = trec_mb_df['raw_message'].str.lower()
 
-"""
-Produce List of Alphabetic Tokens From Raw Text
-"""
+
 def identify_tokens(row):
+    """
+    Produce List of Alphabetic Tokens From Raw Text
+    """
     message = row['raw_message']
     tokens = nltk.word_tokenize(message)
     # taken only words (not punctuation)
     token_words = [w for w in tokens if w.isalpha()]
     return token_words
 
-"""
-Produce Lemmatized List from List of Tokens
-"""
-def lemmatize_list(row):
-    my_list = row['alnum_items']
-    lemmatized_list = [lemmatizer.lemmatize(word) for word in my_list] # TODO: use lemmatizer or stemmer or both(maybe only if doesnt end with "e" or "s" then lemma otherwise stem idk lol)
-    return (lemmatized_list)
 
-"""
-Removes stop words from list of Alphabetic Tokens
-"""
+def lemmatize_list(row):
+    """
+    Produce Lemmatized List from List of Tokens
+    """
+    my_list = row['alnum_items']
+    lemmatized_list = [lemmatizer.lemmatize(word) for word in my_list]
+    return lemmatized_list
+
+
 def remove_stops(row):
+    """
+    Removes stop words from list of Alphabetic Tokens
+    """
     my_list = row['lemmatized_items']
     meaningful_items = [w for w in my_list if w not in stop_words]
-    return (meaningful_items)
+    return meaningful_items
 
-"""
-Rejoin list back to Space Seperated Strings
-"""
+
 def rejoin_words(row):
+    """
+    Rejoin list back to Space Seperated Strings
+    """
     my_list = row['message_content']
-    joined_words = ( " ".join(my_list))
+    joined_words = (" ".join(my_list))
     return joined_words
+
 
 # Create column with only alpha numeric tokens
 trec_mb_df['alnum_items'] = trec_mb_df.apply(identify_tokens, axis=1)
@@ -80,4 +86,3 @@ trec_mb_df.drop(cols_to_drop, axis=1, inplace=True)
 trec_mb_df.to_csv('data/trec_mb_processed.csv', index=False)
 
 # SOURCE: https://pythonhealthcare.org/2018/12/14/101-pre-processing-data-tokenization-stemming-and-removal-of-stop-words/
-
