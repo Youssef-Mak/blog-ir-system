8aea86620fbbe06e6f72e23cdfd56ffa7f7ee2d7
Youssef-Mak <youssef.mak10@gmail.com>
2021-04-03 00:03:46 -0400

    Refined query expansion setup process

--- init.sh
+++ init.sh
@@ -1,1 +1,5 @@
-curl https://dl.fbaipublicfiles.com/fasttext/vectors-english/wiki-news-300d-1M.vec.zip --output ./data/wiki-news-300d-1M.vec.zip && tar -xvf ./data/wiki-news-300d-1M.vec.zip && rm ./data/wiki-news-300d-1M.vec.zip
+cd ./data
+curl https://dl.fbaipublicfiles.com/fasttext/vectors-english/wiki-news-300d-1M.vec.zip --output ./wiki-news-300d-1M.vec.zip 
+tar -xvf ./wiki-news-300d-1M.vec.zip 
+rm ./wiki-news-300d-1M.vec.zip
+
